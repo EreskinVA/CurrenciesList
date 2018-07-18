d684ce0b1566900c855d7da70e2c40cd1a98a181
EreskinVA <ereskinva@icloud.com>
2018-07-18 20:11:59 +0300

    create interface

---
+++ CurrenciesList/CurrencyCell.swift
@@ -0,0 +1,18 @@
+//
+//  CurrencyCell.swift
+//  CurrenciesList
+//
+//  Created by Vladimir Ereskin on 18.07.2018.
+//  Copyright © 2018 Vladimir Ereskin. All rights reserved.
+//
+
+import UIKit
+
+class CurrencyCell: UITableViewCell {
+    
+    @IBOutlet weak var name: UILabel!
+    @IBOutlet weak var volume: UILabel!
+    @IBOutlet weak var amount: UILabel!
+    @IBOutlet weak var numberArray: UILabel!
+    
+}
